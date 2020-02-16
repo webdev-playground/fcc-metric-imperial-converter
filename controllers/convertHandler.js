7da01ef2629ea3eb71a38c431be406be23ca1593
Glitch (fcc-proj-metric-imperial-converter) <none>
2020-02-16 02:18:56 +0000

    💋🛢 Checkpoint ./controllers/convertHandler.js:11249790/1795

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -16,19 +16,68 @@ function ConvertHandler() {
   };
   
   this.getUnit = function(input) {
-    var result = math.evaluate(input).;
+    var result = math.evaluate(input).formatUnits();
     
     return result;
   };
   
   this.getReturnUnit = function(initUnit) {
+    const units  = this.getUnit(initUnit);
     var result;
+    
+    switch (units) {
+      case 'kg':
+        result = 'lbs';
+        break;
+      case 'lbs':
+        result = 'kg';
+        break;
+      case 'km':
+        result = 'mi';
+        break;
+      case 'mi':
+        result = 'km';
+        break;
+      case 'L':
+        result = 'gal';
+        break;
+      case 'gal':
+        result = 'L';
+        break;
+      default:
+        result = '??';
+        break;
+    }
     
     return result;
   };
 
   this.spellOutUnit = function(unit) {
     var result;
+    
+    switch (unit) {
+      case 'kg':
+        result = 'kilogram';
+        break;
+      case 'lbs':
+        result = 'pounds';
+        break;
+      case 'km':
+        result = 'miles';
+        break;
+      case 'mi':
+        result = 'miles';
+        break;
+      case 'L':
+        result = 'liters';
+        break;
+      case 'gal':
+        result = 'gallons';
+        break;
+      default:
+        result = '??';
+        break;
+    }
     
     return result;
   };
@@ -37,7 +86,32 @@ function ConvertHandler() {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
+    
     var result;
+    
+    switch (initUnit) {
+      case 'kg':
+        result = initNum / lbsToKg;
+        break;
+      case 'lbs':
+        result = initNum * ;
+        break;
+      case 'km':
+        result = 'miles';
+        break;
+      case 'mi':
+        result = 'miles';
+        break;
+      case 'L':
+        result = 'liters';
+        break;
+      case 'gal':
+        result = 'gallons';
+        break;
+      default:
+        result = '??';
+        break;
+    }
     
     return result;
   };
